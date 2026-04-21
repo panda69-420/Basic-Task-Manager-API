5fef4e67004761b0df6b4e04c6225f8be3c627bc
Priyanshu Panda <2306300@kiit.ac.in>
2026-04-22 00:00:34 +0530

    fix db config for deployment

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from models import db
 from routes import task_routes
 import os
 
+app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
     'DATABASE_URL', 'sqlite:///tasks.db'
 )
-
-
-app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
